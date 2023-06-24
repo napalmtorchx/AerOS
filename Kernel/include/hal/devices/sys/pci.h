c6e59a48a36083174f96da3f195e6b510efcc19d
napalmtorchx <gamemakermagician@gmail.com>
2023-06-24 04:42:59 -0400

    PCI comments and header

--- Kernel/include/hal/devices/sys/pci.h
+++ Kernel/include/hal/devices/sys/pci.h
@@ -2,4 +2,57 @@
 #include <lib/types.h>
 #include <hal/ports.h>
 #include <debug/debug.h>
-char* get_pci_vendor(uint16_t vendor_id);
+
+// Structure for managing PCI device header
+typedef struct
+{
+    uint8_t  bus;                           // PCI bus number
+    uint8_t  slot;                          // PCI slot number
+    uint8_t  func;                          // PCI function number
+    uint16_t vendor_id;                     // Vendor ID
+    uint16_t device_id;                     // Device ID
+    uint16_t command;                       // Command register
+    uint16_t status;                        // Status register
+    uint8_t  revision_id;                   // Revision ID
+    uint8_t  prog_if;                       // Programming Interface
+    uint8_t  subclass;                      // Subclass code
+    uint8_t  class_code;                    // Class code
+    uint8_t  cache_line_size;               // Cache line size
+    uint8_t  latency_timer;                 // Latency timer
+    uint8_t  header_type;                   // Header type
+    uint8_t  bist;                          // Built-in self-test
+    uint32_t bar0;                          // Base Address Register 0
+    uint32_t bar1;                          // Base Address Register 1
+    uint32_t bar2;                          // Base Address Register 2
+    uint32_t bar3;                          // Base Address Register 3
+    uint32_t bar4;                          // Base Address Register 4
+    uint32_t bar5;                          // Base Address Register 5
+    uint32_t cardbus_cis_pointer;           // CardBus CIS Pointer
+    uint16_t subsystem_vendor_id;           // Subsystem Vendor ID
+    uint16_t subsystem_id;                  // Subsystem ID
+    uint32_t expansion_rom_base_address;    // Expansion ROM Base Address
+    uint8_t  capabilities_pointer;          // Capabilities Pointer
+    uint8_t  reserved[7];                   // Reserved bytes
+    uint8_t  interrupt_line;                // Interrupt Line
+    uint8_t  interrupt_pin;                 // Interrupt Pin
+    uint8_t  min_grant;                     // Minimum Grant
+    uint8_t  max_latency;                   // Maximum Latency
+} pci_device_t;
+
+/// @brief Read a 16-bit word from the PCI configuration space. @param bus The PCI bus number. @param slot The PCI slot number. @param func The PCI function number. @param offset The offset within the PCI configuration space. @return The 16-bit word read from the PCI configuration space.
+uint16_t pci_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+
+/// @brief Write a 16-bit word to the PCI configuration space. @param bus The PCI bus number. @param slot The PCI slot number. @param func The PCI function number. @param offset The offset within the PCI configuration space. @param data The 16-bit word to be written.
+void pci_write_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint16_t data);
+
+///@brief Read a 32-bit double word from the PCI configuration space. @param bus The PCI bus number. @param slot The PCI slot number. @param func The PCI function number. @param offset The offset within the PCI configuration space. @return The 32-bit double word read from the PCI configuration space.
+uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+
+/// @brief Write a 32-bit double word to the PCI configuration space. @param bus The PCI bus number. @param slot The PCI slot number. @param func The PCI function number. @param offset The offset within the PCI configuration space. @param data The 32-bit double word to be written.
+void pci_write_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t data);
+
+/// @brief Read an 8-bit byte from the PCI configuration space. @param bus The PCI bus number. @param slot The PCI slot number. @param func The PCI function number. @param offset The offset within the PCI configuration space. @return The 8-bit byte read from the PCI configuration space.
+uint8_t pci_read_byte(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+
+/// @brief Write an 8-bit byte to the PCI configuration space. @param bus The PCI bus number.@param slot The PCI slot number. @param func The PCI function number.@param offset The offset within the PCI configuration space. @param data The 8-bit byte to be written.
+void pci_write_byte(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint8_t data);
--- Kernel/src/hal/devices/sys/pci.c
+++ Kernel/src/hal/devices/sys/pci.c
@@ -191,41 +191,7 @@ char* search_for_device(uint16_t dev_id,uint16_t ven_id)
     }
    
 }
-//pci device struct
-typedef struct
-{
-    uint8_t bus;
-    uint8_t slot;
-    uint8_t func;
-    uint16_t vendor_id;
-    uint16_t device_id;
-    uint16_t command;
-    uint16_t status;
-    uint8_t revision_id;
-    uint8_t prog_if;
-    uint8_t subclass;
-    uint8_t class_code;
-    uint8_t cache_line_size;
-    uint8_t latency_timer;
-    uint8_t header_type;
-    uint8_t bist;
-    uint32_t bar0;
-    uint32_t bar1;
-    uint32_t bar2;
-    uint32_t bar3;
-    uint32_t bar4;
-    uint32_t bar5;
-    uint32_t cardbus_cis_pointer;
-    uint16_t subsystem_vendor_id;
-    uint16_t subsystem_id;
-    uint32_t expansion_rom_base_address;
-    uint8_t capabilities_pointer;
-    uint8_t reserved[7];
-    uint8_t interrupt_line;
-    uint8_t interrupt_pin;
-    uint8_t min_grant;
-    uint8_t max_latency;
-} pci_device_t;
+
 //pci_list_devices
 void pci_list_devices()
 {
@@ -264,6 +230,7 @@ void pci_list_devices()
                     uint8_t interrupt_line = pci_read_byte(bus, slot, func, 60);
                     uint8_t interrupt_pin = pci_read_byte(bus, slot, func, 61);
                     uint8_t min_grant = pci_read_byte(bus, slot, func, 62);
+                    
                     //printf("PCI device found at bus %d, slot %d, function %d\n", bus, slot, func);
                     //debug log vendor id and device id
                     //printf("%s Vendor ID: %x-%s\n",DEBUG_INFO, vendor_id,get_pci_vendor(vendor_id));
