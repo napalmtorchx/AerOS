efc141e271f624631c8891cf3be35d94c03a0e03
napalmtorchx <gamemakermagician@gmail.com>
2023-06-24 04:31:42 -0400

    Added comments to VM

--- Kernel/include/runtime/executable.h
+++ Kernel/include/runtime/executable.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <lib/types.h>
 
+// type of executable - service can access crucial system functions, but applications are somewhat restricted
 typedef enum
 {
-    EXEC_INVALID,
-    EXEC_SERVICE,
-    EXEC_APPLICATION,
+    EXEC_INVALID,           // invalid executable type
+    EXEC_SERVICE,           // service/driver
+    EXEC_APPLICATION,       // applcation/game/utility
 } EXEC_TYPE;
 
+// Virtual machine executable/program header
 typedef struct
 {
-    char      name[32];
-    uint32_t  type;
-    uint32_t  text_offset;
-    uint32_t  text_sz;
-    uint32_t  data_offset;
-    uint32_t  data_sz;
+    char      name[32];             // name of executable
+    uint32_t  type;                 // type of executable
+    uint32_t  text_offset;          // offset of code
+    uint32_t  text_sz;              // size of code
+    uint32_t  data_offset;          // offset of data
+    uint32_t  data_sz;              // size of data
 } attr_pack executable_t;
